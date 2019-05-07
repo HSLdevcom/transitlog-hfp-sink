486f77fdcbd883f32d0401b9a7fc9ccb7e666262
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-05-07 13:55:21 +0300

    Read DB connection string from file

--- src/main/java/fi/hsl/transitlog/hfp/Main.java
+++ src/main/java/fi/hsl/transitlog/hfp/Main.java
@@ -2,9 +2,13 @@ package fi.hsl.transitlog.hfp;
 
 import com.typesafe.config.Config;
 import fi.hsl.common.config.ConfigParser;
+import fi.hsl.common.config.ConfigUtils;
 import fi.hsl.common.pulsar.PulsarApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
@@ -20,7 +24,8 @@ public class Main {
         MessageProcessor processor = null;
         QueueWriter writer = null;
         try (PulsarApplication app = PulsarApplication.newInstance(config)) {
-            writer = QueueWriter.newInstance(config);
+            final String connectionString = readConnectionString();
+            writer = QueueWriter.newInstance(config, connectionString);
             processor = MessageProcessor.newInstance(app, writer);
             log.info("Starting to process messages");
 
@@ -36,5 +41,24 @@ public class Main {
             }
         }
 
+    }
+
+    private static String readConnectionString() throws Exception {
+        String connectionString = "";
+        try {
+            //Default path is what works with Docker out-of-the-box. Override with a local file if needed
+            final String secretFilePath = ConfigUtils.getEnv("FILEPATH_CONNECTION_STRING")
+                    .orElse("/run/secrets/db_conn_string");
+            connectionString = new Scanner(new File(secretFilePath))
+                    .useDelimiter("\\Z").next();
+        } catch (Exception e) {
+            log.error("Failed to read DB connection string from secrets", e);
+            throw e;
+        }
+
+        if (connectionString.isEmpty()) {
+            throw new Exception("Failed to find DB connection string, exiting application");
+        }
+        return connectionString;
     }
 }
