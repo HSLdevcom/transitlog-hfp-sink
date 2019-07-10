97a7f41727875c56b26bc5fdea0109e69383ca13
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-07-10 15:24:02 +0300

    Ignore event types other than VP

--- src/main/java/fi/hsl/transitlog/hfp/MessageProcessor.java
+++ src/main/java/fi/hsl/transitlog/hfp/MessageProcessor.java
@@ -89,8 +89,12 @@ public class MessageProcessor implements IMessageHandler {
 
         if (TransitdataSchema.hasProtobufSchema(message, TransitdataProperties.ProtobufSchema.HfpData)) {
             Hfp.Data data = Hfp.Data.parseFrom(message.getData());
-            synchronized (queue) {
-                queue.add(data);
+
+            //Ignore event types other than VP until transitlog has support for other event types
+            if (data.getTopic().getEventType() == Hfp.Topic.EventType.VP) {
+                synchronized (queue) {
+                    queue.add(data);
+                }
             }
         }
         else {
